38a49d3e8e7c210f1a62aa98cc55c5d0743d72eb
Kenny Vincent <kennyvincentpro@gmail.com>
2020-02-27 15:20:26 +0100

    Cours n°2

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,16 +4,25 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use App\Form\ArticleType;
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
      */
-    public function index()
+    public function index(ArticleRepository $repo)
     {
+        $article = $repo->findAll();
+
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles'        => $article
         ]);
     }
 
@@ -25,9 +34,58 @@ class BlogController extends AbstractController
     }
 
     /**
-     * @Route("/blog/12", name="blog_show");
+     * @Route("/blog/new", name="blog_create")
      */
-    public function show() {
-        return $this->render('blog/show.html.twig');
+    public function create(Request $request, EntityManagerInterface $manager) {
+
+        $article = new Article();
+
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            
+            $article->setCreatedAt(new \DateTime()); 
+                
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+        }
+
+        return $this->render('blog/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route ("/blog/{id}/edit", name="blog_edit")
+     */
+    public function edit(Article $article, Request $request, EntityManagerInterface $manager) {
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+        }
+
+        return $this->render('blog/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/blog/{id}", name="blog_show")
+     */
+    public function show(Article $article) {
+   
+        return $this->render('blog/show.html.twig', [
+            'article' => $article
+        ]);
     }
 }
